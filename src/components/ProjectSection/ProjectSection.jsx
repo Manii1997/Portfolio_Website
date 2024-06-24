a243e5f78de0b7252b3b05a3faaef0bec79e44f5
Manii1997 <manibct1817@gmail.com>
2024-06-24 13:33:25 +0530

    mern-crud-app-project-added

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,210 +1,12 @@
 import { useState } from "react";
 import { FaLink } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
+import projectsData from "./projectsData.js";
 
 import "./ProjectSection.css";
 
-const PROJECTSDATA = [
-  {
-    title: "React Js",
-    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/react_kigcy9.png",
-    projectLists: [
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717760176/Portfolio-Website/project-2_xtc8pt.png",
-        projectLink: "https://crud-next-app-m.vercel.app/",
-        technology: ["Next Js", "Tailwind CSS", "Local Storage"],
-        sourceCode: "https://github.com/Manii1997/crud-next-app",
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717760176/Portfolio-Website/project-1_bi5uzs.png",
-        projectLink: "https://nxttrendzappm.ccbp.tech/",
-        technology: [
-          "React Js",
-          "JavaScript",
-          "CSS",
-          "Bootstrap",
-          "Routing",
-          "Local Storage",
-          "JWT Token",
-          "Authorization",
-          "Authentication",
-        ],
-        sourceCode: "https://github.com/Manii1997/NxtTrendz",
-        userCredentials: {
-          primeUser: {
-            username: "rahul",
-            password: "rahul@2021",
-          },
-          user: {
-            username: "raja",
-            password: "raja@2021",
-          },
-        },
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717825591/Portfolio-Website/project-3_kcycbw.png",
-        projectLink: "https://rrapp.ccbp.tech/",
-        technology: [
-          "React Js",
-          "Authentication",
-          "React Router",
-          "JWT (JSON Web Tokens)",
-        ],
-        sourceCode: "https://github.com/Manii1997/crud-next-app",
-        userCredentials: {
-          user: {
-            username: "rahul",
-            password: "rahul@2021",
-          },
-        },
-      },
-    ],
-  },
-  {
-    title: "WordPress",
-    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/wordpress_vp0sjd.png",
-    projectLists: [
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-1_drplby.png",
-        projectLink: "https://www.maduraitourstravels.in/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085990/Portfolio-Website/wp-2_iuxm7n.png",
-        projectLink: "https://southernholidays.net/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-3_uuuuzg.png",
-        projectLink: "https://abutoursandtravels.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-4_jiq78l.png",
-        projectLink: "https://cholagold.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-6_k12w94.png",
-        projectLink: "https://kcrinternational.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085994/Portfolio-Website/wp-7_ffpeho.png",
-        projectLink: "https://neotravelmart.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718086176/Portfolio-Website/wp-8_njdyfi.png",
-        projectLink: "https://thanyaas.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-9_k5fjzo.png",
-        projectLink: "https://tcda.in/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-10_wvfpnt.png",
-        projectLink: "https://jprexports.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085990/Portfolio-Website/wp-11_rkhktd.png",
-        projectLink: "https://sihrsjo.in/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718086096/Portfolio-Website/wp-12_zpqwz3.png",
-        projectLink: "https://todosolution.net/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-13_upc4ko.png",
-        projectLink: "https://prinscafe.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-14_ix1nww.png",
-        projectLink: "https://lebanonpackersandmovers.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/Portfolio-Website/wp-15_akupce.png",
-        projectLink: "https://awetechwaterpurifier.in/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/Portfolio-Website/wp-16_k6qdz4.png",
-        projectLink: "https://cleaningguruhomeservices.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/Portfolio-Website/wp-17_crzcay.png",
-        projectLink: "https://bennysbiocoir.com/",
-        technology: ["WordPress", "Elementor"],
-      },
-      {
-        projectImg:
-          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/Portfolio-Website/wp-18_crnr9x.png",
-        projectLink: "https://ssfashions.net/",
-        technology: ["WordPress", "Elementor"],
-      },
-    ],
-  },
-  {
-    title: "Designs",
-    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/web-design_cgd8iy.png",
-    projectLists: [
-      {
-        behance: "https://www.behance.net/embed/project/66758921?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/105896223?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/105896123?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/112583781?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/129236829?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/134490611?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/135252377?ilo0=1",
-      },
-      {
-        behance: "https://www.behance.net/embed/project/148851087?ilo0=1",
-      },
-    ],
-  },
-];
-
 const ProjectSection = () => {
-  const [selectedProject, setSelectedProject] = useState(PROJECTSDATA[0]);
+  const [selectedProject, setSelectedProject] = useState(projectsData[0]);
   const handleSelectProject = (data) => {
     setSelectedProject(data);
   };
@@ -217,7 +19,7 @@ const ProjectSection = () => {
       <div className="p-3 flex flex-col">
         <h1 className="text-4xl md:text-6xl font-bold self-center">Projects</h1>
         <div className="flex flex-wrap mt-10 gap-5 self-center">
-          {PROJECTSDATA.map((item) => (
+          {projectsData.map((item) => (
             <div
               key={item.title}
               className={`project-tab border flex-grow rounded border-[#2196f3] p-4 md:px-5 cursor-pointer ${
@@ -252,7 +54,7 @@ const ProjectSection = () => {
               {list.behance && (
                 <iframe
                   src={list.behance}
-                  allowfullscreen
+                  allowFullScreen
                   height="316"
                   className=" rounded"
                 ></iframe>
@@ -260,10 +62,7 @@ const ProjectSection = () => {
               {list.technology && (
                 <div className="mt-3 flex flex-wrap gap-3">
                   {list.technology.map((tech, techIndex) => (
-                    <p
-                      key={techIndex}
-                      className=" bg-[#0F172A] px-3 py-1.5 rounded-3xl"
-                    >
+                    <p className=" bg-[#0F172A] px-3 py-1.5 rounded-3xl">
                       {tech}
                     </p>
                   ))}
---
+++ src/components/ProjectSection/projectsData.js
@@ -0,0 +1,218 @@
+const projectsData = [
+  {
+    title: "React Js",
+    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/react_kigcy9.png",
+    projectLists: [
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1719215846/Portfolio-Website/project-4_vw4gce.png",
+        projectLink: "https://mern-crud-app-m.vercel.app/",
+        technology: [
+          "React Js",
+          "Tailwind CSS",
+          "MongoDb",
+          "Node Js",
+          "Express",
+          "Routing",
+          "JWT Token",
+          "Authorization",
+          "Authentication",
+        ],
+        sourceCode:
+          "https://github.com/Manii1997/MERN-CRUD-App-with-Login-Logout",
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717760176/Portfolio-Website/project-2_xtc8pt.png",
+        projectLink: "https://crud-next-app-m.vercel.app/",
+        technology: ["Next Js", "Tailwind CSS", "Local Storage"],
+        sourceCode: "https://github.com/Manii1997/crud-next-app",
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717760176/Portfolio-Website/project-1_bi5uzs.png",
+        projectLink: "https://nxttrendzappm.ccbp.tech/",
+        technology: [
+          "React Js",
+          "JavaScript",
+          "CSS",
+          "Bootstrap",
+          "Routing",
+          "Local Storage",
+          "JWT Token",
+          "Authorization",
+          "Authentication",
+        ],
+        sourceCode: "https://github.com/Manii1997/NxtTrendz",
+        userCredentials: {
+          primeUser: {
+            username: "rahul",
+            password: "rahul@2021",
+          },
+          user: {
+            username: "raja",
+            password: "raja@2021",
+          },
+        },
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717825591/Portfolio-Website/project-3_kcycbw.png",
+        projectLink: "https://rrapp.ccbp.tech/",
+        technology: [
+          "React Js",
+          "Authentication",
+          "React Router",
+          "JWT (JSON Web Tokens)",
+        ],
+        sourceCode: "https://github.com/Manii1997/crud-next-app",
+        userCredentials: {
+          user: {
+            username: "rahul",
+            password: "rahul@2021",
+          },
+        },
+      },
+    ],
+  },
+  {
+    title: "WordPress",
+    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/wordpress_vp0sjd.png",
+    projectLists: [
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-1_drplby.png",
+        projectLink: "https://www.maduraitourstravels.in/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085990/Portfolio-Website/wp-2_iuxm7n.png",
+        projectLink: "https://southernholidays.net/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-3_uuuuzg.png",
+        projectLink: "https://abutoursandtravels.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-4_jiq78l.png",
+        projectLink: "https://cholagold.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085991/Portfolio-Website/wp-6_k12w94.png",
+        projectLink: "https://kcrinternational.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085994/Portfolio-Website/wp-7_ffpeho.png",
+        projectLink: "https://neotravelmart.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718086176/Portfolio-Website/wp-8_njdyfi.png",
+        projectLink: "https://thanyaas.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-9_k5fjzo.png",
+        projectLink: "https://tcda.in/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-10_wvfpnt.png",
+        projectLink: "https://jprexports.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085990/Portfolio-Website/wp-11_rkhktd.png",
+        projectLink: "https://sihrsjo.in/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718086096/Portfolio-Website/wp-12_zpqwz3.png",
+        projectLink: "https://todosolution.net/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-13_upc4ko.png",
+        projectLink: "https://prinscafe.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085992/Portfolio-Website/wp-14_ix1nww.png",
+        projectLink: "https://lebanonpackersandmovers.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/wp-15_akupce.png",
+        projectLink: "https://awetechwaterpurifier.in/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/wp-16_k6qdz4.png",
+        projectLink: "https://cleaningguruhomeservices.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/wp-17_crzcay.png",
+        projectLink: "https://bennysbiocoir.com/",
+        technology: ["WordPress", "Elementor"],
+      },
+      {
+        projectImg:
+          "https://res.cloudinary.com/drdl4pdnx/image/upload/v1718085993/wp-18_crnr9x.png",
+        projectLink: "https://ssfashions.net/",
+        technology: ["WordPress", "Elementor"],
+      },
+    ],
+  },
+  {
+    title: "Designs",
+    icon: "https://res.cloudinary.com/drdl4pdnx/image/upload/v1717920021/Portfolio-Website/web-design_cgd8iy.png",
+    projectLists: [
+      {
+        behance: "https://www.behance.net/embed/project/66758921?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/105896223?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/105896123?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/112583781?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/129236829?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/134490611?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/135252377?ilo0=1",
+      },
+      {
+        behance: "https://www.behance.net/embed/project/148851087?ilo0=1",
+      },
+    ],
+  },
+];
+
+export default projectsData;
